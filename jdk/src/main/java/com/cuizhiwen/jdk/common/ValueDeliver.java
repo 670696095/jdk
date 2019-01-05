3a593195a8466d54061429e6ed4c8a09f90819f6
670696095 <670696095@qq.com>
2019-01-05 21:23:41 +0800

    提交

--- jdk/src/main/java/com/cuizhiwen/jdk/common/ValueDeliver.java
+++ jdk/src/main/java/com/cuizhiwen/jdk/common/ValueDeliver.java
@@ -16,21 +16,21 @@ public class ValueDeliver {
         /**
          * 装箱：将基本类型转换成包装类对象
          */
-        int i=10;
+        int i = 10;
         //手动装箱
-        Integer x=new Integer(i);
+        Integer x = new Integer(i);
         //自动装箱
-        Integer y=10;
+        Integer y =10;
 
         /**
          * 拆箱：将包装类对象转换成基本类型的值
          */
-        Integer j=new Integer(8);
+        Integer j = new Integer(8);
         //手动拆箱
-        int m=j.intValue();
+        int m = j.intValue();
 
         //自动拆箱
-        int n=j;
+        int n = j;
     }
 
 }
